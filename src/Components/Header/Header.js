60f90ab08e4a5e6d151b5353e20617d09baa301e
Heather Hartley <hlhartley@gmail.com>
2019-02-20 21:07:53 -0700

    Adds search, icons, and title to Header.js

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Header = () => {
+    return(
+        <div className='header-container'>
+            <div className='search-container'>
+                <input className='search-input'></input>
+                <i class="fas fa-search"></i>
+            </div>
+            <h1 className='title'>Seasonal Shades</h1>
+            <div>
+                <i className="fas fa-user-circle"></i>
+                <i className="fas fa-heart"></i>
+            </div>
+        </div>
+    )
+};
+
