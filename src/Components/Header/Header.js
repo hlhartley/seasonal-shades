ad01b9044aa2ee8356b80f3593ed99c6d5e69775
Heather Hartley <hlhartley@gmail.com>
2019-02-21 14:45:34 -0700

    Makes small SASS changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 export const Header = () => {
     return(
         <div className='header-container'>
-            <div className='search-container'>
+            {/* <div className='search-container'>
                 <input className='search-input'></input>
                 <i class="fas fa-search"></i>
-            </div>
+            </div> */}
             <h1 className='title'>Seasonal Shades</h1>
             <div>
                 <i className="fas fa-user-circle"></i>
