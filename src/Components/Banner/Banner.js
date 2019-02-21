008cf1d910a6742e5aaadf567c900e9fa4e1c9b8
Heather Hartley <hlhartley@gmail.com>
2019-02-21 16:26:02 -0700

    Changes banner styling

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -4,7 +4,7 @@ export const Banner = () => {
     return(
         <div className='banner-container'>
             <img className='banner-img' src={require('../../Images/blush.jpg')} />
-            <p className='banner-text'>Our mission is to find you the perfect shade</p>
+            {/* <p className='banner-text'>Our mission is to find you the perfect shade</p> */}
         </div>
     )
 }
