675523bfab1da6fb7e31985b34b741173d8ba572
Heather Hartley <hlhartley@gmail.com>
2019-02-20 17:11:46 -0700

    Makes dynamic fetch call to makeup API

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import '../../../src/main.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      eyeshadow: [],
+      lipstick: [],
+      blush: [],
+      nail_polish: [],
+    };
+  };
+
+  componentDidMount() {
+    this.fetchMakeup('blush');
+
+  };
+
+  fetchMakeup = async (path) => {
+    const url = `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${path}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    const items = result.map((item) => {
+      return item
+    });
+    this.setState({ [`${path}`]: items })
+  };
+
   render() {
     return (
       <div className="App">
@@ -9,8 +34,8 @@ class App extends Component {
           App
         </header>
       </div>
-    );
-  }
-}
+    )
+  };
+};
 
 export default App;
