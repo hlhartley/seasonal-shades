c59a6a068ba4487ecb5378769e4a1f9f6d96d8f8
Heather Hartley <hlhartley@gmail.com>
2019-02-23 14:08:54 -0700

    Adds overall styling to page

--- src/Containers/Navigation/Navigation.js
+++ src/Containers/Navigation/Navigation.js
@@ -4,8 +4,12 @@ import { fetchMakeup } from '../../Thunks/fetchMakeup';
 import { NavLink } from 'react-router-dom';
 
 export class Navigation extends Component {
+    state = {
+        activeLink: 'eyeshadow',
+    }
 
     handleClick = async (type) => {
+        this.setState({activeLink: type})
         this.props.updateCurrentType(type)
         await this.props.fetchMakeup(type)
     }
@@ -13,10 +17,10 @@ export class Navigation extends Component {
     render() {
         return (
             <div className='navigation-container'>
-                <NavLink to='/eyeshadow' onClick={() => this.handleClick('eyeshadow')}>EYES</NavLink>
-                <NavLink to='/lipstick' onClick={() => this.handleClick('lipstick')}>LIPS</NavLink>
-                <NavLink to='/blush' onClick={() => this.handleClick('blush')}>CHEEKS</NavLink>
-                <NavLink to='/nail_polish' onClick={() => this.handleClick('nail_polish')}>NAILS</NavLink>
+                <NavLink to='/eyeshadow' className={this.state.activeLink === 'eyeshadow' ? 'is-active' : 'not-active'} onClick={() => this.handleClick('eyeshadow')}>EYES</NavLink>
+                <NavLink to='/lipstick' className={this.state.activeLink === 'lipstick' ? 'is-active' : 'not-active'} onClick={() => this.handleClick('lipstick')}>LIPS</NavLink>
+                <NavLink to='/blush' className={this.state.activeLink === 'blush' ? 'is-active' : 'not-active'} onClick={() => this.handleClick('blush')}>CHEEKS</NavLink>
+                <NavLink to='/nail_polish' className={this.state.activeLink === 'nail_polish' ? 'is-active' : 'not-active'} onClick={() => this.handleClick('nail_polish')}>NAILS</NavLink>
             </div>
         )
     }
