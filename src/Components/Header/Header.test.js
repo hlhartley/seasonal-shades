be602047ac992750464e1e12443de3ff9a819a79
Heather Hartley <hlhartley@gmail.com>
2019-02-25 16:30:33 -0700

    Adds mapStateToProps tests for Search and Header components

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import Header from './Header';
+import Header, { mapStateToProps } from './Header';
 import { shallow } from 'enzyme';
+import { mockFavorites, mockState } from '../../__fixtures__/mockData';
 
-describe('Header component', () => {
+describe('Header', () => {
     let wrapper;
-    beforeEach(() => {
-        wrapper = shallow(<Header />)
+    describe('Header component', () => {
+        beforeEach(() => {
+            wrapper = shallow(<Header favorites={mockFavorites}/>)
+        });
+    
+        it('should match the snapshot with all the data passed in correctly', () => {
+            expect(wrapper).toMatchSnapshot()
+        });
     });
 
-    it('should match the snapshot with all the data passed in correctly', () => {
-        expect(wrapper).toMatchSnapshot()
+    describe('mapStateToProps', () => {
+        it(' should return an object with all favorites', () => {
+            const expected = {
+                favorites: {
+                    '001lune': true,
+                    '001petal': true, 
+                }
+            }
+            const mappedProps = mapStateToProps(mockState)
+            expect(mappedProps).toEqual(expected)
+        });
     });
 });
+
--- src/Components/Search/Search.test.js
+++ src/Components/Search/Search.test.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import Search from './Search';
+import Search, { mapStateToProps } from './Search';
 import { shallow } from 'enzyme';
+import { mockState } from '../../__fixtures__/mockData';
 
-describe('Search Component', () => {
-    let wrapper;
-    let mockHandleChange;
-
-    beforeEach(() => {
-        mockHandleChange = jest.fn()
-        wrapper = shallow(<Search handleChange={mockHandleChange}/>)
+describe('Search', () => {
+    describe('Search Component', () => {
+        let wrapper;
+        let mockHandleChange;
+    
+        beforeEach(() => {
+            mockHandleChange = jest.fn()
+            wrapper = shallow(<Search handleChange={mockHandleChange} lipstickColors={[]} blushColors={undefined} eyeshadowColors={undefined} nail_polishColors={undefined}/>)
+        });
+    
+        it('should take a snapshot and pass in the data correctly', () => {
+            expect(wrapper).toMatchSnapshot()
+        });
     });
 
-    it('should take a snapshot and pass in the data correctly', () => {
-        expect(wrapper).toMatchSnapshot()
+    describe('mapStateToProps', () => {
+        const expected = {
+            blushColors: undefined,
+            eyeshadowColors: undefined,
+            lipstickColors: [],
+            nail_polishColors: undefined,
+        }
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
     });
 });
